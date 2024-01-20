e15c5c3825a7722ab5a31019a80f9fb81224e437
Yingying Li <yli@slack-corp.com>
2024-01-19 17:08:21 -0800

    save progress

---
+++ datastores/url.ts
@@ -0,0 +1,26 @@
+import { SlackAPIClient } from "deno-slack-api/types.ts";
+
+type UrlItem = {
+  name: string;
+  url: string;
+};
+
+export default class UrlDatastore {
+  private static readonly DATASTORE_NAME = "url";
+
+  static get = async (
+    client: SlackAPIClient,
+    name: string,
+  ): Promise<string> => {
+    console.log(
+      `Getting ${name} URL from the datastore...`,
+    );
+    const ret = await client.apps.datastore.get({
+      datastore: this.DATASTORE_NAME,
+      id: name,
+    });
+    if (!ret.ok) throw new Error(ret.error);
+    const datastoreItem = ret.item as UrlItem;
+    return datastoreItem.url ?? "";
+  };
+}
