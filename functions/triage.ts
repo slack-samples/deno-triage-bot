4bbf1ab9e6e55bc5b1702f52f143e8f434d56d51
David Frankel <42774874+frankeld@users.noreply.github.com>
2024-08-28 19:37:51 -0400

    Allow numbers in type

--- functions/triage.ts
+++ functions/triage.ts
@@ -69,7 +69,7 @@ type MessageType = {
   type: string;
   ts: string;
   text: string;
-  attachments?: { [key: string]: string };
+  attachments?: { [key: string]: string | number };
   team: string;
   user?: string;
   username?: string;
@@ -293,7 +293,8 @@ function isRequest(
     ? message.comment?.comment ?? message.text
     : message.text;
 
-  const msg_attachment_text = message.attachments && message.attachments["text"]
+  const msg_attachment_text = message.attachments &&
+      typeof message.attachments["text"] === "string"
     ? message.attachments["text"]
     : "";
 
